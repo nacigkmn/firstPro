3fb16fa2a8d66e5a826455ea2e70c5adbb535a05
Naci Gokmen <nacigokmen99@gmail.com>
2023-12-17 00:50:09 +0100

    sec commit

--- src/test/java/erdem/LogMedimops.java
+++ src/test/java/erdem/LogMedimops.java
@@ -4,69 +4,117 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
 
 public class LogMedimops {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\nacig\\Desktop\\web\\prosject\\firefox\\geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        loginToMedimops(driver,"naci2575@gmail.com","naci@1234");
-        System.out.println("hello world");
-
+        loginToMedimops(driver, "************", "*****");
+        //driver.get("https://www.medimops.de/MeinMerkzettel/");
+        //acceptCookies(driver);
+        wait(2000);
+        clearBrowserCache(driver);
+        addFavBooksToCart(driver);
+       // addToCart(driver);
+        //getAvaibleBook(driver);
     }
 
 
-    public static void clearBrowserCache(WebDriver webDriver) throws InterruptedException {
+    public static void clearBrowserCache(WebDriver webDriver) {
         webDriver.manage().deleteAllCookies();
-        Thread.sleep(3000);
-        //wait(3000);
+
+        wait(2000);
     }
 
-    private static void loginToMedimops(WebDriver driver, String username, String password) throws InterruptedException {
+    private static void loginToMedimops(WebDriver driver, String username, String password) {
         // Giriş yapma sayfasına git
 
         driver.get("https://www.medimops.de/Mein-Konto/");
         clearBrowserCache(driver);
         acceptCookies(driver);
 
-       WebElement button_username = driver.findElement(By.id("lgn_usr"));
-       button_username.sendKeys(username);
-        Thread.sleep(3000);
-       WebElement button_password = driver.findElement(By.id("password"));
-       button_password.sendKeys(password);
-        Thread.sleep(3000);
+        WebElement button_username = driver.findElement(By.id("lgn_usr"));
+        button_username.sendKeys(username);
+        wait(3000);
+        WebElement button_password = driver.findElement(By.id("password"));
+        button_password.sendKeys(password);
+        wait(3000);
 
         driver.findElement(By.cssSelector(".login-form__submit.btn.btn--primary.btn--fluid")).submit();
+    }
 
+    private static void addFavBooksToCart(WebDriver driver) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement buttonToFavBooks = driver.findElement(By.cssSelector("a.dashboard__button-cta.dashboard__button-cta--list"));
+        buttonToFavBooks.click();
+
+        List<WebElement> addToCartButtons = driver.findElements(By.xpath("//a[contains(text(), 'In den Warenkorb')]"));
+        for (WebElement addToCartButton : addToCartButtons) {
+            addToCartButton.click();
+            System.out.println(addToCartButton.getAttribute("class")+"sepete eklendi.");
+        }
 
     }
 
+    private  static  void getAvaibleBook(WebDriver driver){
 
-    public static void acceptCookies(WebDriver driver) throws InterruptedException {
+        List<WebElement> elements = driver.findElements(By.className("notice-list-product__grid "));
+        elements.forEach(x-> System.out.println(x));
+    }
+
+
+
+
+
+    private static void addToCart(WebDriver driver) {
+        // Tüm "a" elementlerini bul
+        List<WebElement> addToCartButtons = driver.findElements(By.cssSelector("a.add-to-cart.add-to-cart__main"));
+
+        // Her bir "a" elementine tıkla
+        for (WebElement addToCartButton : addToCartButtons) {
+            // Bağlantının metnini kontrol et
+            if (addToCartButton.getText().contains("In den Warenkorb")) {
+                addToCartButton.click();
+                System.out.println("Ürün sepete eklendi.");
+
+            }
+        }
+    }
+
+
+
+
+    public static void acceptCookies(WebDriver driver) {
         WebElement button = driver.findElement(By.id("accept-all"));
         button.click();
-        Thread.sleep(1000);
-        // wait(1000);
+        wait(3000);
+
     }
 
-    private static void acceptCokies(WebDriver driver) throws InterruptedException {
+    private static void acceptCokies(WebDriver driver) {
         WebElement button = driver.findElement(By.id("accept-all"));
         button.click();
-        Thread.sleep(1000);
-       // wait(1000);
-    }
-
-
-    public static void clearBrowsercache(WebDriver webDriver) throws InterruptedException {
-        webDriver.manage().deleteAllCookies();
-        Thread.sleep(1000);
-       // wait(3000);
+        wait(3000);
 
     }
 
 
-    public static void wait(int interval) {
+
+    private static void wait(int interval) {
+        try {
+            Thread.sleep(interval);
+        } catch (InterruptedException e) {
+            System.out.println("Error in waiting");
+            throw new RuntimeException(e);
+        }
 
     }
 
--- src/test/java/erdem/Master.java
+++ src/test/java/erdem/Master.java
@@ -10,72 +10,85 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 public class Master {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\nacig\\Desktop\\web\\prosject\\firefox\\geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.medimops.de/Mein-Konto/");
+        loginToMedimops(driver, "naci2575@gmail.com", "naci@1234");
+        System.out.println("hello world");
+    }
 
+    public static void clearBrowserCache(WebDriver webDriver) {
+        webDriver.manage().deleteAllCookies();
+        wait(3000);
+
+    }
+
+    private static void loginToMedimops(WebDriver driver, String username, String password) {
+
+
+        driver.get("https://www.medimops.de/Mein-Konto/");
         clearBrowserCache(driver);
         acceptCookies(driver);
 
+        WebElement button_username = driver.findElement(By.id("lgn_usr"));
+        button_username.sendKeys(username);
+        wait(3000);
+        WebElement button_password = driver.findElement(By.id("password"));
+        button_password.sendKeys(password);
+        wait(3000);
 
-        loginToAmazon(driver,"naci2575@gmail.com","naci@1234");
+        driver.findElement(By.cssSelector(".login-form__submit.btn.btn--primary.btn--fluid")).submit();
 
-        searchProduct(driver,"Shark AV2501AE AI Robot Vacuum with XL HEPA Self-Empty Base, Bagless," +
-                " 60-Day Capacity, LIDAR Navigation, Perfect for Pet Hair, Compatible with Alexa, Wi-Fi Connected, " +
-                "Carpet & Hard Floor, Black");
-        addProductToCart(driver,"Shark AV2501AE AI Robot Vacuum with XL HEPA Self-Empty Base, Bagless," +
-                " 60-Day Capacity, LIDAR Navigation, Perfect for Pet Hair, Compatible with Alexa, Wi-Fi Connected, " +
-                "Carpet & Hard Floor, Black");
-        //printCartContents(driver);
 
     }
 
-    private static void loginToAmazon(WebDriver driver, String username, String password) {
-        // Giriş yapma sayfasına git
-        driver.findElement(By.id("nav-link-accountList")).click();
-        driver.findElement(By.id("ap_email")).sendKeys(username);
-        driver.findElement(By.id("continue")).click();
-        driver.findElement(By.id("ap_password")).sendKeys(password);
-        driver.findElement(By.id("signInSubmit")).click();
-    }
 
-    public static void clearBrowserCache(WebDriver webDriver) throws InterruptedException {
-        webDriver.manage().deleteAllCookies();
-        Thread.sleep(3000);
-        //wait(3000);
-    }
-
-    public static void acceptCookies(WebDriver driver) throws InterruptedException {
+    public static void acceptCookies(WebDriver driver)  {
         WebElement button = driver.findElement(By.id("accept-all"));
         button.click();
-        Thread.sleep(3000);
-       // wait(1000);
+        wait(3000);
+
     }
 
-        private static void searchProduct(WebDriver driver, String productName) {
-            // Arama kutusunu bul ve ürün adını gönder
-            WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
-            searchBox.sendKeys(productName);
+    private static void acceptCokies(WebDriver driver) {
+        WebElement button = driver.findElement(By.id("accept-all"));
+        button.click();
+        wait(3000);
 
-            // Arama düğmesine tıkla
-            WebElement searchButton = driver.findElement(By.id("nav-search-submit-button"));
-            searchButton.click();
-        }
-    /*
-        private static void addProductToCart(WebDriver driver, String productName) throws InterruptedException {
-
-            WebElement addToCartButton = driver.findElement(By.xpath("//span[contains(text(), '" + productName + "')]/ancestor::div[@id='add-to-cart']//button"));
-            addToCartButton.click();
+    }
 
 
-            WebElement proceedToCheckoutButton = driver.findElement(By.id("proceed-to-checkout-button"));
-            proceedToCheckoutButton.click();
-            Thread.sleep(3000);
-        }
-    */
+    public static void clearBrowsercache(WebDriver webDriver)  {
+        webDriver.manage().deleteAllCookies();
+        wait(3000);
+
+    }
+
+
+
+    private static void searchAddProductToCart(WebDriver driver, String productName)  {
+
+        WebElement searchBox = driver.findElement(By.id("search__text-field"));
+        searchBox.sendKeys(productName);
+
+        // Arama düğmesine tıkla
+        WebElement searchButton = driver.findElement(By.cssSelector(".search__submit"));
+        searchButton.click();
+
+        WebElement addToCartButton = driver.findElement(By.xpath("//span[contains(text(), '" + productName + "')]/ancestor::div[@id='add-to-cart']//button"));
+        addToCartButton.click();
+
+        // Bekleme ekleyebilirsiniz
+        wait(3000);
+
+        WebElement proceedToCheckoutButton = driver.findElement(By.id("proceed-to-checkout-button"));
+        proceedToCheckoutButton.click();
+        printCartContents(driver);
+      wait(3000);
+    }
+
     private static void printCartContents(WebDriver driver) {
         // Sepet içeriğini bulmak için uygun CSS veya XPath kullanın
         List<WebElement> cartItems = driver.findElements(By.cssSelector(".cart-item")); // Örnek bir CSS seçici, gerçek CSS'i kontrol etmelisiniz
@@ -87,17 +100,14 @@ public class Master {
         }
     }
 
-    private static void addProductToCart(WebDriver driver, String productName) throws InterruptedException {
-        WebElement addToCartButton = driver.findElement(By.xpath("//span[contains(text(), '" + productName + "')]/ancestor::div[@id='add-to-cart']//button"));
-        addToCartButton.click();
+    private  static void wait(int interval){
+        try {
+            Thread.sleep(interval);
+        } catch (InterruptedException e) {
+            System.out.println("Error in waiting");
+            throw new RuntimeException(e);
+        }
 
-        // Bekleme ekleyebilirsiniz
-        Thread.sleep(3000);
-
-        WebElement proceedToCheckoutButton = driver.findElement(By.id("proceed-to-checkout-button"));
-        proceedToCheckoutButton.click();
-        printCartContents(driver);
-        Thread.sleep(3000);
     }
 
 
